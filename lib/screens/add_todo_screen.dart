2b497010d34d910fd7fa8dfeae87d98516ab7500
aimenhassan223-svg <aimenhassan223@gmail.com>
2026-09-13 20:09:37 +0500

    Screen to add new todos

---
+++ lib/screens/add_todo_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/todo.dart';
+
+class AddTodoScreen extends StatefulWidget {
+  const AddTodoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddTodoScreen> createState() => _AddTodoScreenState();
+}
+
+class _AddTodoScreenState extends State<AddTodoScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  void _saveTodo() {
+    if (_formKey.currentState!.validate()) {
+      context.read<TodoProvider>().addTodo(
+            _titleController.text,
+            _descriptionController.text,
+          );
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Todo'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter todo title',
+                ),
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter todo description',
+                ),
+                maxLines: 4,
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: _saveTodo,
+                  child: const Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text('Add Todo'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
